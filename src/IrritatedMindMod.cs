5f1064585a00a8e12b678b141c77ea8933347b4d
Gasterbuzzer <101058373+Gasterbuzzer@users.noreply.github.com>
2025-01-21 20:46:04 +0100

    Init Repository

---
+++ src/IrritatedMindMod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EasyIrritatedMind.src;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(IrritatedMindMod), "IrritatedMindMod", "1.0.0", "Gasterbuzzer", "https://github.com/Gasterbuzzer/EasyIrritatedMind/releases/")]
+[assembly: MelonGame("Burlaka Kyrylo", "Irritated Mind Fear of Warehouse")]
+[assembly: MelonAuthorColor(ConsoleColor.Magenta)]
+[assembly: MelonPriority(99)]
+
+namespace EasyIrritatedMind.src
+{
+
+    public static class BuildInfo
+    {
+        public const string Name = "EasyIrritatedMind";
+        public const string Description = "Mod for making the game easier.";
+        public const string Author = "Gasterbuzzer";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/Gasterbuzzer/EasyIrritatedMind/releases/";
+    }
+
+    public class IrritatedMindMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg($"Easy IrritatedMind Mod has been loaded!");
+        }
+    }
+}
